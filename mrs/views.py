eb0232aadd4d75e5fb33ae856aefc298a6dbdd48
Artem <artela1992@gmail.com>
2020-11-26 17:12:52 +0300

    расширение возможностей views

--- mrs/views.py
+++ mrs/views.py
@@ -18,6 +18,10 @@ class DeanViews(ModelViewSet):
     queryset = Dean.objects.all()
     serializer_class = DeanSerializer
 
+    @action(detail=True, methods=['get'])
+    def groups(self, request, pk=None):
+        return Response(data=GroupSerializer(instance=self.get_object().group_set.all(), many=True).data)
+
 
 class StudentViews(ModelViewSet):
     queryset = Student.objects.all()
@@ -32,12 +36,24 @@ class TeacherViews(ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
 
+    @action(detail=True, methods=['get'])
+    def subjects(self, request, pk=None):
+        return Response(data=SubjectSerializer(instance=self.get_object().subject_set.all(), many=True).data)
+
 
 class SubjectViews(ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
+    @action(detail=True, methods=['get'])
+    def students(self, request, pk=None):
+        return Response(data=StudentSerializer(instance=self.get_object().students.all(), many=True).data)
+
 
 class GroupViews(ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+    @action(detail=True, methods=['get'])
+    def students(self, request, pk=None):
+        return Response(data=StudentSerializer(instance=self.get_object().student_set.all(), many=True).data)
